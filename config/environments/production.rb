c123f8d33b949b026a5736bbeee7ebc4caf87753
John Ragan <jpragan16@gmail.com>
2017-02-15 20:17:04 +0000

    updated paperclip configuration

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -81,6 +81,7 @@ config.action_mailer.default_url_options = { host: ' pinterested1616.herokuapp.c
 
 config.paperclip_defaults = {
   :storage => :s3, 
+  :s3_region => ENV['AWS_REGION'],
   :s3_credentials => {
   :bucket => ENV['AWS_BUCKET'],
     :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
